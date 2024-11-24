88562ac2600c31cee7e36824acc0b0743f61e916
Luiza Hackenhaar Naziazeno <142232479+luizahackenhaarnaziazeno@users.noreply.github.com>
2024-11-24 05:42:17 +0000

    faltando alguns metodos

--- App.java
+++ App.java
@@ -1,43 +1,220 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
         ArvoreBinariaPesquisa abp = new ArvoreBinariaPesquisa();
-        abp.adicionar(48);
+        abp.adicionar(15);
+        abp.adicionar(9);
+        abp.adicionar(2);
+        abp.adicionar(11);
+        abp.adicionar(23);
         abp.adicionar(20);
-        abp.adicionar(87);
-        abp.adicionar(53);
-        abp.adicionar(90);
-        abp.adicionar(89);
-        abp.adicionar(93);
-        abp.adicionar(10);
-        abp.adicionar(30);
-        abp.adicionar(25);
-        abp.adicionar(32);
-        System.out.println("");
-        System.out.println("");
-        abp.imprimirArvore();
+        abp.adicionar(28);
 
-        System.out.println("Removendo 21"); 
-        abp.remover(21);
-        abp.imprimirArvore();
+        while (continuar) {
+            System.out.println("\n === Menu ===");
+            System.out.println("1 - Imprindo Árvore Criada:");
+            System.out.println("2 -Tamanho Da Árvore Criada:");
+            System.out.println("3 - Obter Nodo Da Esquerda Do 9:");
+            System.out.println("4 - Obter Nodo Da Direita Do 23:");
+            System.out.println("5 - Pai Do 20:");
+            System.out.println("6 - Se o valor 11 existe na árvore:");
+            System.out.println("7 - Se o valor 12 existe na árvore:");
+            System.out.println("8 - Se o valor 2 é interno:");
+            System.out.println("9 - Se o valor 23 é externo:");
+            System.out.println("10 - Maior valor da árvore:");
+            System.out.println("11 - Menor valor da árvore:");
+            System.out.println("12 - Quantidade de nodos não folhas na árvore:");
+            System.out.println("13 - Quantidade de nodos folhas na árvore:");
+            System.out.println("14 - Preordem:");
+            System.out.println("15 - Posordem:");
+            System.out.println("16 - OrdemEmLargura:");
+            System.out.println("17 - Centralordem:");
+            System.out.println("18 - Nivel do Nodo 11:");
+            System.out.println("19 - Altura do Nodo 15:");
+            System.out.println("20 - A quantidades de números pares na árvore:");
+            System.out.println("21 - A quantidades de números ímpares na árvore:");
+            System.out.println("22 - Remover 2:");
+            System.out.println("23 - Limpar a árvore:");
+            System.out.println("24 - Sair:");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Removendo 20");
-        abp.remover(20);
-        abp.imprimirArvore();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Imprimindo árvore criada:");
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("Tamanho = " + abp.obterTamanho());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 3:
+                    abp.imprimirArvore();
+                    System.out.println("Obter Nodo Da Esquerda Do 9");
+                    System.out.println(abp.obterEsquerda(9));
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 4:
+                    abp.imprimirArvore();
+                    System.out.println("Obter Nodo Da Direita Do 23");
+                    System.out.println(abp.obterDireita(23));
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 5:
+                    int valor = 20;
+                    System.out.println("Pai do " + valor + " = " + abp.obterPai(20));
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 6:
+                    int valor2 = 11;
+                    abp.imprimirArvore();
+                    System.out.println("Se " + valor2 + " existe na árvore");
+                    System.out.println(abp.existe(11));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 7:
+                    int valor3 = 12;
+                    abp.imprimirArvore();
+                    System.out.println("Se " + valor3 + " existe na árvore");
+                    System.out.println(abp.existe(12));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 8:
+                    int valor4 = 2;
+                    System.out.println("Se " + valor4 + " é interno ");
+                    System.out.println(abp.ehInterno(2));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 9:
+                    int valor5 = 23;
+                    System.out.println("Se " + valor5 + " é externo");
+                    System.out.println(abp.ehExterno(23));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 10:
+                    System.out.println("Maior valor da árvore é ");
+                    System.out.println(abp.maiorValor());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 11:
+                    System.out.println("Menor valor da árvore é ");
+                    System.out.println(abp.menorValor());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 12:
+                    System.out.println("Quantidade de nodos não folhas na árvore é ");
+                    System.out.println(abp.qtdNosNaoFolha());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 13:
+                    System.out.println("Quantidade de nodos folhas na árvore é ");
+                    System.out.println(abp.qtdNodosFolha());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 14:
+                    abp.imprimirArvore();
+                    System.out.println("Preordem = " + abp.elementosPreOrdem());
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 15:
+                    abp.imprimirArvore();
+                    System.out.println("Posordem = " + abp.elementosPosOrdem());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 16:
+                    abp.imprimirArvore();
+                    System.out.println("OrdemEmLargura = " + abp.elementosLargura());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 17:
+                    abp.imprimirArvore();
+                    System.out.println("Centralordem = " + abp.elementosCentralOrdem());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 18:
+                    int valor6 = 11;
+                    abp.imprimirArvore();
+                    System.out.println("Nivel do Nodo " + valor6 + " é " + "=");
+                    System.out.println(abp.obterNivel(11));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 19:
+                    int valor7 = 15;
+                    abp.imprimirArvore();
+                    System.out.println("Altura do Nodo " + valor7 + " é " + "=");
+                    System.out.println(abp.obterAltura(15));
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 20:
+                    abp.imprimirArvore();
+                    System.out.println("A quantidades de números pares na árvore é ");
+                    System.out.println(abp.qtdPares());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 21:
+                    abp.imprimirArvore();
+                    System.out.println("A quantidades de números ímpares na árvore é ");
+                    System.out.println(abp.qtdImpares());
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 22:
+                    System.out.println("Removendo 2");
+                    abp.remover(2);
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 23:
+                    System.out.println("Limpando a árvore");
+                    abp.limpar();
+                    abp.imprimirArvore();
+                    System.out.println("");
+                    System.out.println("");
+                    break;
+                case 24:
+                    System.out.println("Saindo...");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+        /*
+         * System.out.println("");
+         * System.out.println("Raiz = " + abp.obterRaiz());
+         */
 
-        /*System.out.println("");
-        System.out.println("Raiz = " + abp.obterRaiz());
-        int c1 = 53;
-        System.out.println("Pai do " + c1 + " = " + abp.obterPai(53));
-
-        System.out.println("Tamanho = " + abp.obterTamanho());
-        System.out.println("Preordem = " + abp.percorrerPreOrdem());
-
-        System.out.println(abp.obterAltura(93));
-
-        System.out.println(abp.contarNodos());
-        System.out.println(abp.maiorValor());
-        System.out.println(abp.maiorValor_gpt());
-        */
     }
 }
--- ArvoreBinariaPesquisa.java
+++ ArvoreBinariaPesquisa.java
@@ -3,189 +3,313 @@ public class ArvoreBinariaPesquisa {
     private class ResultadosBuscaProfundidade {
         public int tamanho;
         public String preordem;
+        public String centralordem;
+        public String posordem;
+        public String largura;
+
         public ResultadosBuscaProfundidade() {
             tamanho = 0;
             preordem = "";
+            centralordem = "";
+            posordem = "";
+            largura = "";
         }
 
     }
-    /*
-fazer:
-arrumar o método remover para ele tirar o nodo quando ele tiver na arvore 
-• int obterEsquerda(int item)
-• int obterDireita(int item)
-• boolean existe(int item)
-• String elementosCentralOrdem()
-• String elementosPosOrdem()
-• String elementosLargura()
-boolean ehInterno(int item)
-• boolean ehExterno(int item)
-• int qtdNosNaoFolha() //retorna a quantidade de nodos não folha
-• int qtdNodosFolha() //quantidade de nodos folha
-int altura()-utilizar obter nivel
-• int obterNivel(int item)-acho
-• int obterMenorValor() //retorna a menor chave da árvore
-• int qtdPares() //retorna a quantidade de chaves que são pares
-• int qtdImpares() //retorna a quantidade de chaves são impares
-• void mergear(ArvoreBinariaPesquisa t) //esse método deve inserir todas as chaves da árvore t na árvore atual
-(a do objeto que está disparando esse método)
-• adicionar(String lista) //método para adicionar todas as chaves contidas em lista
-• remover(String lista) //método para remover todas as chaves contidas em lista. Esse parâmetro é um string que
-deve enviar as chaves a serem removidas separadas por vírgula. Exemplo: “34, 99, 12”. Remove 34, 99, 12
-• gerarArvoreAleatoria(int n) //método que limpa a árvore atual e adiciona n chaves não repetidas na árvore
-
-
-
-
- */
 
     private class Nodo {
         private int item;
         private Nodo esquerda;
         private Nodo direita;
         private Nodo pai;
+
         public Nodo(int item) {
             this.item = item;
         }
     }
+
     private Nodo raiz;
-    public ArvoreBinariaPesquisa() {};
+
+    public ArvoreBinariaPesquisa() {
+    };
+
+    /*
+    *Caminhamento em largura
+     * • void mergear(ArvoreBinariaPesquisa t) //esse método deve inserir todas as
+     * chaves da árvore t na árvore atual
+     * (a do objeto que está disparando esse método)
+     * • adicionar(String lista) //método para adicionar todas as chaves contidas em
+     * lista
+     * • remover(String lista) //método para remover todas as chaves contidas em
+     * lista. Esse parâmetro é um string que
+     * deve enviar as chaves a serem removidas separadas por vírgula. Exemplo: “34,
+     * 99, 12”. Remove 34, 99, 12
+     * • gerarArvoreAleatoria(int n) //método que limpa a árvore atual e adiciona n
+     * chaves não repetidas na árvore
+     */
+
+    // Métodos que estão funcionando
+
     public void adicionar(int item) {
         Nodo n = new Nodo(item);
-        if(estaVazia()) {
+        if (estaVazia()) {
             raiz = n;
-        }
-        else {
+        } else {
             adicionarRecursivo(n, raiz);
         }
     }
+
     private void adicionarRecursivo(Nodo n, Nodo pai) {
-        if(n.item>pai.item) {
-            if(pai.direita==null) {
+        if (n.item > pai.item) {
+            if (pai.direita == null) {
                 pai.direita = n;
                 n.pai = pai;
-            }
-            else adicionarRecursivo(n, pai.direita);
-        }
-        else {
-            if(pai.esquerda==null) {
+            } else
+                adicionarRecursivo(n, pai.direita);
+        } else {
+            if (pai.esquerda == null) {
                 pai.esquerda = n;
                 n.pai = pai;
             }
 
-            else adicionarRecursivo(n, pai.esquerda);
+            else
+                adicionarRecursivo(n, pai.esquerda);
         }
     }
 
-
-    public void remover(int item) {
-     
-        Nodo n = obterNodoRecursivo(item, raiz);
-        if(n==null) {
-        System.out.println(" não foi possivel remover o"+ item);
-        }else{
-        if(numeroFilhos(n)==0) {
-            if(n.pai.esquerda==n) n.pai.esquerda = null;
-            else n.pai.direita = null;
-        }
-        System.out.println(item + " removido com sucesso");
-        }
-    }
-
-
-
-
-    private int numeroFilhos(Nodo n) {
-        if(n==null) return -1;
-        if(n.esquerda==null && n.direita==null) return 0;
-        if(n.esquerda!=null && n.direita!=null) return 2;
-        return 1;
-    }
-    private Nodo obterNodoRecursivo(int item, Nodo n) {
-        if(n==null) return null;
-        if(item==n.item) return n;
-        if(item>n.item) return obterNodoRecursivo(item, n.direita);
-        if(item<n.item) return obterNodoRecursivo(item, n.esquerda);
-        return null;
-    }
-    public void imprimirArvore() {
-        imprimirArvoreRecusivamente(raiz, 0);
-    }
-    private void imprimirArvoreRecusivamente(Nodo raiz, int nivel) {
-        if (raiz == null) return;
-        nivel += 5;
-        imprimirArvoreRecusivamente(raiz.direita, nivel);
-        System.out.print("\n");
-        for (int i = 5; i < nivel; i++) System.out.print(" ");
-        System.out.print(raiz.item + "\n");
-        for (int i = 5; i < nivel; i++) System.out.print(" ");
-        imprimirArvoreRecusivamente(raiz.esquerda, nivel);
-    }
     public boolean estaVazia() {
-        return raiz==null;
+        return raiz == null;
     }
 
     private void percorrerRecursivamente(Nodo aux, ResultadosBuscaProfundidade resultado) {
-        if(aux==null) return;
+        if (aux == null)
+            return;
         resultado.tamanho++;
         resultado.preordem = resultado.preordem + " " + aux.item;
-        if(aux.esquerda!=null) percorrerRecursivamente(aux.esquerda, resultado);
-        if(aux.direita!=null) percorrerRecursivamente(aux.direita, resultado);
+        percorrerRecursivamente(aux.esquerda, resultado);
+        percorrerRecursivamente(aux.direita, resultado);
     }
-    //Ver se tá funcionado
+
     public int obterTamanho() {
         ResultadosBuscaProfundidade r = new ResultadosBuscaProfundidade();
         percorrerRecursivamente(raiz, r);
         return r.tamanho;
     }
+
     public void limpar() {
         raiz = null;
     }
-    public int obterRaiz() {
-        if(!estaVazia()) return raiz.item;
-        return -1;
+
+    private Nodo obterNodoRecursivo(int item, Nodo n) {
+        if (n == null)
+            return null;
+        if (item == n.item)
+            return n;
+        if (item > n.item)
+            return obterNodoRecursivo(item, n.direita);
+        if (item < n.item)
+            return obterNodoRecursivo(item, n.esquerda);
+        return null;
     }
+
+    public int obterEsquerda(int item) {
+        Nodo n = obterNodoRecursivo(item, raiz);
+        if (n == null) {
+            return -1;
+        } else if (n.esquerda == null)
+            return 0;
+        else
+            return n.esquerda.item;
+    }
+
+    
+    public int obterDireita(int item) {
+    Nodo n = obterNodoRecursivo(item, raiz);
+     if(n==null) return -1;
+     if(n.direita==null) return 0;
+    return n.direita.item;
+     }
+
     public int obterPai(int item) {
-        if(estaVazia()) return -1;
-        if(item==raiz.item) return -1;
+        if (estaVazia())
+            return -1;
+        if (item == raiz.item)
+            return -1;
         Nodo aux = obterNodoRecursivo(item, raiz);
         return aux.pai.item;
     }
-    public String percorrerPreOrdem() {
+
+    public String elementosPreOrdem() {
         ResultadosBuscaProfundidade r = new ResultadosBuscaProfundidade();
         percorrerRecursivamente(raiz, r);
         return r.preordem;
+    }
 
+    private void percorrerRecursivamenteCentral(Nodo aux, ResultadosBuscaProfundidade resultado) {
+        if (aux == null) {
+            return;
+        }
+        percorrerRecursivamenteCentral(aux.esquerda, resultado);
+        resultado.tamanho++;
+        resultado.centralordem = resultado.centralordem + " " + aux.item;
+        percorrerRecursivamenteCentral(aux.direita, resultado);
     }
-    public String percorrerPosOrdem() {
-        return null;
+
+    public String elementosCentralOrdem() {
+        ResultadosBuscaProfundidade r = new ResultadosBuscaProfundidade();
+        percorrerRecursivamenteCentral(raiz, r);
+        return r.centralordem;
     }
-    public String percorrerCentral() {
-        return null;
+
+    private void percorrerRecursivamentePosOrdem(Nodo aux, ResultadosBuscaProfundidade resultado) {
+        if (aux == null) {
+            return;
+        }
+        percorrerRecursivamentePosOrdem(aux.esquerda, resultado);
+        percorrerRecursivamentePosOrdem(aux.direita, resultado);
+        resultado.tamanho++;
+        resultado.posordem = resultado.posordem + " " + aux.item;
     }
-    public String percorrerLargura() {
-        return null;
+
+    public String elementosPosOrdem() {
+        ResultadosBuscaProfundidade r = new ResultadosBuscaProfundidade();
+        percorrerRecursivamentePosOrdem(raiz, r);
+        return r.posordem;
     }
+
+    private void percorrerRecursivamenteEmLargura(Nodo aux, ResultadosBuscaProfundidade resultado) {
+        if (aux == null)
+            return;
+        resultado.tamanho++;
+        resultado.largura = resultado.largura + " " + aux.item;
+        percorrerRecursivamenteEmLargura(aux.esquerda, resultado);
+        percorrerRecursivamenteEmLargura(aux.direita, resultado);
+    }
+
+    // String elementosLargura()
+    // nao esta ok
+    public String elementosLargura() {
+        ResultadosBuscaProfundidade r = new ResultadosBuscaProfundidade();
+        percorrerRecursivamenteEmLargura(raiz, r);
+        return r.largura;
+    }
+
     public int obterNivel(int item) {
-        return 0;
+        return obterNivelRecursivo(item, raiz, 0);
     }
+
+    private int obterNivelRecursivo(int item, Nodo n, int nivel) {
+        if (n == null) {
+            return -1;
+        }
+        if (item == n.item) {
+            return nivel;
+        }
+        if (item < n.item) {
+            return obterNivelRecursivo(item, n.esquerda, nivel + 1);
+        } else {
+            return obterNivelRecursivo(item, n.direita, nivel + 1);
+        }
+    }
+
+    public boolean existe(int item) {
+        if (obterNodoRecursivo(item, raiz) != null)
+            return true;
+        else
+            return false;
+    }
+
     public int obterAltura(int item) {
-        return 0;
+        return obterAlturaRecursivo(raiz);
     }
+
+    private int obterAlturaRecursivo(Nodo n) {
+        if (n == null) {
+            return -1;
+        }
+        int alturaEsquerda = obterAlturaRecursivo(n.esquerda);
+        int alturaDireita = obterAlturaRecursivo(n.direita);
+        return Math.max(alturaEsquerda, alturaDireita) + 1;
+    }
+
     public boolean ehInterno(int item) {
-        return false;
-    }
-    public boolean ehExterno(int item) {
+        Nodo n = obterNodoRecursivo(item, raiz);
+        if (n != null && numeroFilhos(n) > 0) {
+            return true;
+        }
         return false;
     }
 
-    public int contarNodos() {
+    private int numeroFilhos(Nodo n) {
+        if (n == null)
+            return -1;
+        if (n.esquerda == null && n.direita == null)
+            return 0;
+        if (n.esquerda != null && n.direita != null)
+            return 2;
+        return 1;
+    }
+
+    public boolean ehExterno(int item) {
+        Nodo n = obterNodoRecursivo(item, raiz);
+        if (n != null && numeroFilhos(n) > 1) {
+            return true;
+        }
+        return false;
+    }
+
+    
+    public void remover(int item) {
+        Nodo n = obterNodoRecursivo(item, raiz);
+        if (n == null) {
+            System.out.println(" não foi possivel remover o" + item);
+        } else {
+            if (numeroFilhos(n) == 0) {
+                if (n.pai.esquerda == n)
+                    n.pai.esquerda = null;
+                else
+                    n.pai.direita = null;
+            }
+            System.out.println(item + " removido com sucesso");
+        }
+    }
+
+ 
+      public int qtdNosNaoFolha() {
+      return contarNodos()-qtdNodosFolha();
+      }
+ 
+      public int contarNodos() {
         return contarNodosRecursivo(raiz);
     }
+
     private int contarNodosRecursivo(Nodo n) {
         if(n==null) return 0;
         return 1 + contarNodosRecursivo(n.esquerda) + contarNodosRecursivo(n.direita);
     }
+     
+
+    public int qtdNodosFolha() {
+        return contarNodosFolhasRecursivo(raiz);
+    }
+
+    private int contarNodosFolhasRecursivo(Nodo n) {
+        if (n == null)
+            return 0;
+        if (n.esquerda == null && n.direita == null)
+            return 1;
+        return contarNodosFolhasRecursivo(n.esquerda) + contarNodosFolhasRecursivo(n.direita);
+    }
+
+    private void maiorValorRecursivo(Nodo n, int[] maior) {
+        if (n == null)
+            return;
+        if (n.item > maior[0])
+            maior[0] = n.item;
+        maiorValorRecursivo(n.esquerda, maior);
+        maiorValorRecursivo(n.direita, maior);
+    }
+
     public int maiorValor() {
         int[] maior = new int[1];
         maior[0] = Integer.MIN_VALUE;
@@ -193,31 +317,69 @@ deve enviar as chaves a serem removidas separadas por vírgula. Exemplo: “34, 
         return maior[0];
     }
 
-    //• int obterMaiorValor() //retorna a maior chave da árvore
-    private void maiorValorRecursivo(Nodo n, int[] maior) {
-        if(n==null) return;
-        if(n.item>maior[0]) maior[0] = n.item;
-        maiorValorRecursivo(n.esquerda, maior);
-        maiorValorRecursivo(n.direita, maior);
+    private void menorValorRecursivo(Nodo n, int[] menor) {
+        if (n == null)
+            return;
+        if (n.item < menor[0])
+            menor[0] = n.item;
+        menorValorRecursivo(n.esquerda, menor);
+        menorValorRecursivo(n.direita, menor);
     }
 
-    public int maiorValor_gpt() {
-        return maiorValorRecursivo_gpt(raiz);
+    public int menorValor() {
+        int[] menor = new int[1];
+        menor[0] = Integer.MAX_VALUE;
+        menorValorRecursivo(raiz, menor);
+        return menor[0];
     }
 
-    private int maiorValorRecursivo_gpt(Nodo n) {
-        if (n == null) {
-            return Integer.MIN_VALUE;
-        }
-        int esquerdaMax = maiorValorRecursivo_gpt(n.esquerda);
-        int direitaMax = maiorValorRecursivo_gpt(n.direita);
-        return Math.max(n.item, Math.max(esquerdaMax, direitaMax));
+    public int qtdPares() {
+        return qtdParesRecursivo(raiz);
     }
+
+    public int qtdParesRecursivo(Nodo n) {
+        if (n == null)
+            return 0;
+        if (n.item % 2 == 0)
+            return 1 + qtdParesRecursivo(n.esquerda) + qtdParesRecursivo(n.direita);
+        return qtdParesRecursivo(n.esquerda) + qtdParesRecursivo(n.direita);
+    }
+
     
-    public int contarFilhosDaDireita() {
-        return 0;
+    public int qtdImpares() {
+        return qtdImparesRecursivo(raiz);
     }
 
- 
+    public int qtdImparesRecursivo(Nodo n) {
+        if (n == null)
+            return 0;
+        if (n.item % 2 != 0)
+            return 1 + qtdImparesRecursivo(n.esquerda) + qtdImparesRecursivo(n.direita);
+        return qtdImparesRecursivo(n.esquerda) + qtdImparesRecursivo(n.direita);
+    }
+
+    public void imprimirArvore() {
+        imprimirArvoreRecusivamente(raiz, 0);
+    }
+
+    private void imprimirArvoreRecusivamente(Nodo raiz, int nivel) {
+        if (raiz == null)
+            return;
+        nivel += 5;
+        imprimirArvoreRecusivamente(raiz.direita, nivel);
+        System.out.print("\n");
+        for (int i = 5; i < nivel; i++)
+            System.out.print(" ");
+        System.out.print(raiz.item + "\n");
+        for (int i = 5; i < nivel; i++)
+            System.out.print(" ");
+        imprimirArvoreRecusivamente(raiz.esquerda, nivel);
+    }
+
+    public int obterRaiz() {
+        if (!estaVazia())
+            return raiz.item;
+        return -1;
+    }
 
 }
